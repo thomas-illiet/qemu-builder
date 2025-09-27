c299e1341656b56007e398d8b5fb7c26b44a8cd0
Thomas ILLIET <contact@thomas-illiet.fr>
2025-09-27 22:50:37 +0200

    Update build_alpine.sh

--- scripts/build_alpine.sh
+++ scripts/build_alpine.sh
@@ -2,15 +2,15 @@
 set -e
 
 # Variables
-IMAGE_NAME="alpine-arm64-docker.qcow2"
+IMAGE_NAME="alpine-amd64-docker.qcow2"
 IMAGE_SIZE="2G"
 ALPINE_VERSION="3.22.0"
-MINIROOTFS="alpine-minirootfs-${ALPINE_VERSION}-aarch64.tar.gz"
+MINIROOTFS="alpine-minirootfs-${ALPINE_VERSION}-x86_64.tar.gz"
 ROOTFS_DIR="rootfs"
 
 # Télécharger minirootfs si nécessaire
 if [ ! -f "$MINIROOTFS" ]; then
-    curl -LO https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/aarch64/$MINIROOTFS
+    curl -LO https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/$MINIROOTFS
 fi
 
 # Créer rootfs
